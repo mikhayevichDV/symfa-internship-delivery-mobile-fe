61d2f1ee3159ed1618f0b20f88e2b6dff753c4e2
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-04-06 11:34:03 +0300

    feat: Auth and Client layouts

--- src/app/layouts/client-layout/client-layout.tsx
+++ src/app/layouts/client-layout/client-layout.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import './style.scss';
 
 export const ClientLayout: React.FC = () => {
-  return <div className="client-layout">Client</div>;
+  return (
+    <div className="client-layout">
+      <Outlet />
+    </div>
+  );
 };
